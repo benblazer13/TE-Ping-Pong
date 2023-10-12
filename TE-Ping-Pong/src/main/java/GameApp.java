61d793195a1f7fb38656398c8b2c42ef1ca2989a
Benjamin Hoffman <benblazer2020@gmail.com>
2023-10-11 22:08:40 -0400

    repaired system UI and created game modes

--- TE-Ping-Pong/src/main/java/GameApp.java
+++ TE-Ping-Pong/src/main/java/GameApp.java
@@ -1,39 +1,27 @@
 import Profiles.Player;
-import Profiles.PlayerData;
 import TournamentSystem.Game;
 
-import javax.print.DocFlavor;
 import java.util.Scanner;
 
 public class GameApp {
     private final Scanner userInput = new Scanner(System.in);
 
-    private PlayerData data = new PlayerData();
+    Game game = new Game();
+
     public static void main(String[] args) {
         GameApp gameApp = new GameApp();
         gameApp.runGame();
     }
 
-
-
     public void runGame(){
 
-        Game game = new Game();
 
         game.setPlayer1Score(0);
         game.setPlayer2Score(0);
 
 
         System.out.println("Welcome to PingPong");
-//        System.out.println("Please select a game mode");
-//        System.out.println("[1] PingPong to 11");
-//        System.out.println("[2] PingPong to 21");
-//        String gameMode = userInput.nextLine();
-//
-//
-//        System.out.println("Enter number of matches: ");
-//        String matchNumber = userInput.nextLine();
-//        int numberOfMatches = Integer.parseInt(matchNumber);
+
 
         System.out.println("Who is Player 1?");
         String player1input = userInput.nextLine();
@@ -43,21 +31,80 @@ public class GameApp {
         game.setPlayer1(player1);
        /* Player player1 =
 
-       i need to take the string and take it into a player
-        eventually want this to turn into a txt file searcher
+
+        TODO eventually want this to turn into a txt file searcher
         game.setPlayer1(player1);
        */
-                System.out.println("Who is Player 2?");
+                System.out.println("Who is Player 2?"); // hello
         String player2input = userInput.nextLine();
 
 
         Player player2 = new Player(player2input, null, null, null);
         game.setPlayer2(player2);
 
-        game.isGameStillActive();
+        System.out.println("Please select a game mode");
+        System.out.println("[1] PingPong to 11");
+        System.out.println("[2] PingPong to 21");
+        String gameMode = userInput.nextLine();
+        int gameEnd = 0;
 
+        while(true) {
+            try {
+                if(gameMode.equals("1")){
+                    gameEnd = 11;
+                    break;
+                } else if (gameMode.equals("2")) {
+                    gameEnd = 21;
+                    break;
+                }else{
+                    System.out.println("Invalid Selection..");
+                    System.out.println("Try Again");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Something went wrong, try again");
+            }
+        }
+        //TODO create gameMode input
+
+        System.out.println("Enter number of matches: ");
+        String matchNumber = userInput.nextLine();
+        int numberOfMatches = Integer.parseInt(matchNumber);
+
+        while(game.isGameStillActive(gameEnd)){
+            scorePoint();
+            System.out.println("----------------------------");
+            System.out.println(game.getPlayer1().getFirstName() + ") " + game.getPlayer1Score());
+            System.out.println(game.getPlayer2().getFirstName() + ") " + game.getPlayer2Score());
+            System.out.println("----------------------------");
+        }
+        //while loop, call scorePoint within loop
+
+
+        //game.getWinner
+        System.out.println("The victor is: " + game.getWinner().getFirstName() + "!");
+        System.out.println();
 
     }
+
+    public void scorePoint(){
+        while(true) {
+            System.out.println("Which Player scored:");
+            System.out.println("1. " + game.getPlayer1().getFirstName());
+            System.out.println("2. " + game.getPlayer2().getFirstName());
+
+            String whoScored = userInput.nextLine();
+
+            try {
+                game.scorePoint(whoScored);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid Input");
+                System.out.println("Try again:");
+            }
+        }
+    }
+
+
 
 
 }
--- TE-Ping-Pong/src/main/java/TournamentSystem/Game.java
+++ TE-Ping-Pong/src/main/java/TournamentSystem/Game.java
@@ -51,47 +51,45 @@ public class Game {
     public void setPlayer2(Player player2) {
         this.player2 = player2;
     }
+    //TODO CLI is the only place for sout
+    public void scorePoint(String whoScored){
 
-    public void scorePoint(){
-        System.out.println("Which Player scored:");
-        System.out.println("1. " + getPlayer1().getFirstName());
-        System.out.println("2. " + getPlayer2().getFirstName());
-
-        String userInput = scanner.nextLine();
-
-        if(userInput.equalsIgnoreCase("1")){
+        if(whoScored.equalsIgnoreCase("1")){
             //player1 goes up by 1; counter++
             player1Score ++;
             pointTracker.add(player1);
-        } else if (userInput.equalsIgnoreCase("2")) {
+        } else if (whoScored.equalsIgnoreCase("2")) {
             //player2 goes up by 2; counter++
             pointTracker.add(player2);
             player2Score++;
+        }else{
+            throw new IllegalArgumentException();
         }
     }
 
-    public boolean isGameStillActive(){
-
+    public boolean isGameStillActive(int gameEnd){
+        //make constants for game type variables
         boolean isGameActive = true;
-        while (isGameActive){
 
-            if(getPlayer1Score() >= 21 & getPlayer1Score() > getPlayer2Score() + 1){
-                System.out.println("The victor is: " + getPlayer1().getFirstName() + "!");
-                System.out.println();
+
+            if(getPlayer1Score() >= gameEnd & getPlayer1Score() > getPlayer2Score() + 1){
 
                 isGameActive = false;
-            } else if (getPlayer2Score() >= 21 & getPlayer2Score() > getPlayer1Score() + 1) {
-                System.out.println("The victor is: " + getPlayer2().getFirstName() + "!");
-                System.out.println();
+            } else if (getPlayer2Score() >= gameEnd & getPlayer2Score() > getPlayer1Score() + 1) {
 
                 isGameActive = false;
-            }else {
-                scorePoint();
             }
-            System.out.println(getPlayer1().getFirstName() + ") " + getPlayer1Score());
-            System.out.println(getPlayer2().getFirstName() + ") " + getPlayer2Score());
+        return isGameActive;
+    }
+
+
+    public Player getWinner(){
+
+        if(getPlayer1Score() > getPlayer2Score()){
+            return player1;
+        }else {
+            return player2;
         }
-        return false;
 
     }
 
