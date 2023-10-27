148c365a37eb3d78cc6aaee947b72d988d766887
Benjamin Hoffman <benblazer2020@gmail.com>
2023-10-27 18:37:54 -0400

    added more jdbc methods

--- TE-Ping-Pong/src/main/java/Profiles/JdbcPlayerDao.java
+++ TE-Ping-Pong/src/main/java/Profiles/JdbcPlayerDao.java
@@ -1,30 +1,73 @@
 package Profiles;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcPlayerDao {
 
-    private final JdbcTemplate
+    private final JdbcTemplate jdbcTemplate;
 
-    int getPlayerCount(){
+    public JdbcPlayerDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int getPlayerCount(){
         int playerCount = 0;
         String sql = "SELECT COUNT(*) AS count FROM player;";
 
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        if (results.next()){
+            playerCount = results.getInt("count");
+        }
 
         return playerCount;
     }
 
 
+     public List<String> getPlayerNames(){
+        List<String> playerNames = new ArrayList<>();
+        String sql = "SELECT first_name FROM player order by first_name asc;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()){
+            playerNames.add(results.getString("first_name"));
+        }
+        return playerNames;
+     }
+     public Player getPlayerByFirstName(String name){
+        Player player = null;
+        String sql = "SELECT * FROM player where first_name = ?;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, name);
+        if(results.next()){
+            player = mapRowToPlayer(results);
+        }
+        return player;
+     }
 
 
 
-//    List<String> getPlayerNames();
-//    Player getPlayer();
+
+
+
+
 //    Player getPlayerById(int playerId);
 //    List<Player> getPlayersByWins();
 //    Player createPlayer();
 //    Player updatePlayer();
 //    int deletePlayerById(int playerId);
 
+
+    private Player mapRowToPlayer (SqlRowSet rowSet){
+        Player player = new Player();
+        player.setFirstName(rowSet.getString("first_name"));
+        player.setLastName(rowSet.getString("last_name"));
+        player.setDescription(rowSet.getString("description"));
+        player.setNickname(rowSet.getString("nickname"));
+        return player;
+    }
 
 }
